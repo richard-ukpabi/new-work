5face625a144beeb9818e22b789ca452cbd0ed30
Richard <richard-ukpabi25@gmail.com>
2026-02-19 11:08:11 +0000

    adding the codes to remote repo

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+// Serve everything inside the public folder
+app.use(express.static('public'));
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
